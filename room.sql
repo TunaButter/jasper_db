d80da990c039ea9058ef5354d083728691380a75
TunaButter <yunwei.li@carleton.ca>
2019-02-17 07:50:29 -0500

    Add files via upload

---
+++ room.sql
@@ -0,0 +1,22 @@
+delete from rooms;
+
+insert into rooms (name, location, seats, capacity, telephone, calendar)
+values('311A(E)', '3', 12, 37, '613-960-6464', 'AQMkADAwATM3ZmYAZS0xZWQyLWU0ZDEtMDACLTAwCgBGAAADuaOGRiaWJUeLSSjCrWPpCwcAIgcoqDGsOEGJ8e9NcrEd7QAAAgEGAAAAIgcoqDGsOEGJ8e9NcrEd7QAAAg0oAAAA');
+
+insert into rooms (name, location, seats, capacity, telephone, calendar)
+values('399B(W)', '3', 5, 12, '613-960-3498', 'AQMkADAwATM3ZmYAZS0xZWQyLWU0ZDEtMDACLTAwCgBGAAADuaOGRiaWJUeLSSjCrWPpCwcAIgcoqDGsOEGJ8e9NcrEd7QAAAgEGAAAAIgcoqDGsOEGJ8e9NcrEd7QAAAg0pAAAA');
+
+insert into rooms (name, location, seats, capacity, telephone, calendar)
+values('401A(W)', '4', 10, 20, '613-960-4354', 'AQMkADAwATM3ZmYAZS0xZWQyLWU0ZDEtMDACLTAwCgBGAAADuaOGRiaWJUeLSSjCrWPpCwcAIgcoqDGsOEGJ8e9NcrEd7QAAAgEGAAAAIgcoqDGsOEGJ8e9NcrEd7QAAAg0qAAAA');
+
+insert into rooms (name, location, seats, capacity, telephone, calendar)
+values('708A(E)', '7', 7, 11, '613-960-9238', 'AQMkADAwATM3ZmYAZS0xZWQyLWU0ZDEtMDACLTAwCgBGAAADuaOGRiaWJUeLSSjCrWPpCwcAIgcoqDGsOEGJ8e9NcrEd7QAAAgEGAAAAIgcoqDGsOEGJ8e9NcrEd7QAAAg0sAAAA');
+
+insert into rooms (name, location, seats, capacity, telephone, calendar)
+values('830B(E)', '8', 4, 5, '613-960-3847', 'AQMkADAwATM3ZmYAZS0xZWQyLWU0ZDEtMDACLTAwCgBGAAADuaOGRiaWJUeLSSjCrWPpCwcAIgcoqDGsOEGJ8e9NcrEd7QAAAgEGAAAAIgcoqDGsOEGJ8e9NcrEd7QAAAg0tAAAA');
+
+insert into rooms (name, location, seats, capacity, telephone, calendar)
+values('515A(E)', '5', 6, 8, '613-960-9874', 'AQMkADAwATM3ZmYAZS0xZWQyLWU0ZDEtMDACLTAwCgBGAAADuaOGRiaWJUeLSSjCrWPpCwcAIgcoqDGsOEGJ8e9NcrEd7QAAAgEGAAAAIgcoqDGsOEGJ8e9NcrEd7QAAAg0rAAAA');
+
+insert into rooms (name, location, seats, capacity, telephone, calendar)
+values('530B(W)', '5', 10, 10, '613-960-4687', 'AQMkADAwATM3ZmYAZS0xZWQyLWU0ZDEtMDACLTAwCgBGAAADuaOGRiaWJUeLSSjCrWPpCwcAIgcoqDGsOEGJ8e9NcrEd7QAAAgEGAAAAIgcoqDGsOEGJ8e9NcrEd7QAAAg0uAAAA');
